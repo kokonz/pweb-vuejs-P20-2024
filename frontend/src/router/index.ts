8400c44db1b81513afe52a20fd8366e3d7e96532
kokon <rafaelgunawanrafael@gmail.com>
2024-11-29 23:43:27 +0700

    add book

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import BooksView from '../views/BooksView.vue';
 import BookDetails from '../components/BookDetails.vue';
+import AddBook from '../components/AddBook.vue'; // Import AddBook component
 
 const routes = [
   {
@@ -12,7 +13,12 @@ const routes = [
     path: '/book/:book',
     name: 'BookDetails',
     component: BookDetails,
-  }
+  },
+  {
+    path: '/add-book',
+    name: 'AddBook',
+    component: AddBook,
+  },
 ];
 
 const router = createRouter({
